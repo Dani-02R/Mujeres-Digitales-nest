251ea25e031523fab818804b54fa91527093ce0c
Dani-02R <da933522@gmail.com>
2025-10-01 17:51:25 -0500

    Subo proyecto Nest con módulo de productos

--- mi-app-nest/mi-app-nest/src/interfaces/IProducts.ts
+++
@@ -1,1 +0,0 @@
-export type IProducts = { id: number, name: string, description: string, price: number }
--- mi-app-nest/mi-app-nest/src/interfaces/IUser.ts
+++
@@ -1,7 +0,0 @@
-export type IUser = { 
-    id: number, 
-    name: string, 
-    email: string, 
-    password: string, 
-    age?: number 
-}
